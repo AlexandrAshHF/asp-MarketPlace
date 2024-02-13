439f5383233c6c3f16d9167ef1f0508569edd806
AlexandrAsh <ashevskya@gmail.com>
2024-02-13 13:06:31 +0300

    add jwt provider

--- Back-end/MarketPlace/MarketPlace.Infrastructure/JwtProvider.cs
+++ Back-end/MarketPlace/MarketPlace.Infrastructure/JwtProvider.cs
@@ -1,7 +1,9 @@
 ﻿using MarketPlace.Application.Configurations;
 using MarketPlace.Core.Interfaces.Auth;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace MarketPlace.Infrastructure
 {
@@ -19,7 +21,18 @@ namespace MarketPlace.Infrastructure
             if(!sellerId.IsNullOrEmpty())
                 claims.Append(new Claim("sellerId", sellerId));
 
-            return "";
+            var signInCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.key)),
+                    SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                    signingCredentials: signInCredentials,
+                    expires: DateTime.UtcNow.AddHours(_options.lifetime),
+                    claims: claims);
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
         }
     }
 }
