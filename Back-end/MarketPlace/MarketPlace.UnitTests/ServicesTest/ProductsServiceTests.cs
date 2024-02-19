a5d467487e1a633f58e93ecd05b5022dde7a49d6
AlexandrAsh <ashevskya@gmail.com>
2024-02-19 13:18:51 +0300

    Created context to tests and unit tests to productsService

--- Back-end/MarketPlace/MarketPlace.UnitTests/ServicesTest/ProductsServiceTests.cs
+++ Back-end/MarketPlace/MarketPlace.UnitTests/ServicesTest/ProductsServiceTests.cs
@@ -4,6 +4,7 @@ using MarketPlace.Core.Interfaces.Repositories;
 using MarketPlace.DAL.Repositories;
 using MarketPlace.UnitTests.Common;
 using Xunit;
+using Xunit.Sdk;
 
 namespace MarketPlace.UnitTests.ServicesTest
 {
@@ -21,19 +22,23 @@ namespace MarketPlace.UnitTests.ServicesTest
         [MemberData(nameof(DataToAddProductTest))]
         public async Task AddProductTest_Success(Guid id, string title, string? typeName,
             string desc, List<string> imgList, decimal price,
-            Guid CategoryId, Guid SellerId, Guid expectedGuid, string expectedString)
+            Guid CategoryId, Guid SellerId, Guid expectedGuid)
         {
             var result = await _productsService.AddProductAsync(id, title, typeName, desc, imgList, price, CategoryId, SellerId);
 
             Assert.Equal(expectedGuid, result.Item1);
-            Assert.Equal(expectedString, result.Item2);
         }
         public static IEnumerable<object[]> DataToAddProductTest()
         {
             Guid id = Guid.NewGuid();
             yield return new object[] { id, "testTitle", "TestTypeName", "TestDescription",
                 new List<string>(), 102.3M, ContextFactory.CategoryAId, ContextFactory.SellerAId,
-                id, string.Empty};
+                id};
+            id = Guid.NewGuid();
+            yield return new object[] { id, string.Empty, "TestTypeName", "TestDescription",
+                new List<string>(), 102.3M, ContextFactory.CategoryAId, ContextFactory.SellerAId,
+                Guid.Empty};
         }
+
     }
 }
